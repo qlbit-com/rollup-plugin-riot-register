69bbb317ec22f9fbd8bf4f20fe9b71aa6c4604cd
Dmitriy <dmitriy@qlbit.com>
2023-08-27 20:45:45 -0400

    rollup build added

---
+++ rollup.config.mjs
@@ -0,0 +1,11 @@
+import resolve from '@rollup/plugin-node-resolve'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: 'dist/index.cjs.js', format: 'cjs', exports: 'default' },
+    { file: 'dist/index.es.js',  format: 'es' }
+  ],
+  plugins: [resolve()],
+  external: ['fast-glob']
+}
